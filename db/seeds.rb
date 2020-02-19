d22819f0749a4a0f55e83a177f131537c12ac0d1
maxence desaules <max.desaules@gmail.com>
2020-02-19 19:57:44 +0100

    ended wo style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:  'french',
+    phone_number:  '04567896578'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:  'italian',
+    phone_number:  '04567896578'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
